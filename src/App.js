6433fea8edfedea7cc3472891c4ec5a167bf4a18
Binay Maharjan <bnaysm@gmail.com>
2021-04-26 18:36:54 +0545

    created react app with webpack from scratch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function App() {
+    return (
+        <div>
+            <h1>
+                <h5>
+
+                Welcome { new Date().toDateString() }
+                </h5>
+                </h1>
+        </div>
+    );
+}
